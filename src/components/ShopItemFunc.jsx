3eae63d97d1e24c9fe656ae5a25c578a82958364
Maksim <tkachenkonino1996@gmail.com>
2020-12-23 22:46:04 +0300

    first commit

---
+++ src/components/ShopItemFunc.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import ShopItem from '../models/ShopItem';
+
+export const ShopItemFunc = ({ item }) => {
+  const {
+    brand,
+    title,
+    description,
+    descriptionFull,
+    price,
+    currency
+  } = item;
+
+  return (
+    <div className="main-content">
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+      <div className="description">{descriptionFull}</div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{currency}{`${price}.00`}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  )
+}
+
+ShopItemFunc.propTypes = {
+  item: PropTypes.instanceOf(ShopItem) && PropTypes.shape({
+    brand: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    descriptionFull: PropTypes.string,
+    price: PropTypes.number,
+    currency: PropTypes.string
+  }),
+}
